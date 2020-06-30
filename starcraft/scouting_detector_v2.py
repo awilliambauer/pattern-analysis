77674917df5a392217fb8d960dc3f29baaa3df92
acameron13 <63213231+acameron13@users.noreply.github.com>
2020-06-30 15:20:13 -0700

    revised script - version 2

--- starcraft/scouting_detector_v2.py
+++ starcraft/scouting_detector_v2.py
@@ -1,45 +1,17 @@
 #Alison Cameron
-#May 2020
+#June 2020
 #A program to detect scouting behavior of players in StarCraft 2
 
 import sc2reader
 import math
 
-team1 = 1
-team2 = 2
+def buildEventDictionaries(tracker_events, game_events):
+    '''Builds a list of all relevant events for scouting detection'''
 
-# Dictionaries where the keys are the frame and the value is the state of
-# scouting. "No scouting" indicates the team/player is not looking at any bases,
-# "Scouting themself" indicates the team/player is looking at their own base, and
-# "Scouting opponent" indicates the team/player is looking at their opponent's base
-team1_scouting_states = {}
-team2_scouting_states = {}
-
-#Dictionaries of the times at which each team switches their scouting state.
-#These are intended to assist in the verification process
-team1_times = {}
-team2_times = {}
-
-# Dictionaries of the locations of bases where the keys are unit ids
-# and the values are locations (as tuples of (x, y) coordinates)
-team1_bases = {}
-team2_bases = {}
-
-#r = sc2reader.load_replay("myReplays/ggtracker_335956.SC2Replay", load_map = True)
-#r = sc2reader.load_replay("myReplays/ggtracker_336367.SC2Replay", load_map = True)
-#r = sc2reader.load_replay("myReplays/ggtracker_341316.SC2Replay", load_map = True)
-r = sc2reader.load_replay("replays/Deathaura.SC2Replay", load_map = True)
-#r = sc2reader.load_replay("myReplays/Submarine.SC2Replay", load_map = True)
-#r = sc2reader.load_replay("myReplays/terranMove.SC2Replay", load_map = True)
-tracker_events = r.tracker_events
-game_events = r.game_events
-frames = r.frames
-
-#lists of unit init events and camera events
-unit_init_events = []
-camera_events = []
-
-def buildEventDictionaries():
+    unit_init_events = []
+    camera_events = []
+    team1 = 1
+    team2 = 2
     start1 = False
     start2 = False
     for t_event in tracker_events:
@@ -53,37 +25,54 @@ def buildEventDictionaries():
                 start2 = True
         elif isinstance(t_event, sc2reader.events.tracker.UnitInitEvent) and (t_event.unit.name == "Hatchery" or t_event.unit.name == "CommandCenter" or t_event.unit.name == "Nexus"):
             unit_init_events.append(t_event)
-        #accounting for Terrans moving their bases
-        elif isinstance(t_event, sc2reader.events.tracker.UnitTypeChangeEvent) and t_event.unit.name == "CommandCenter":
-            unit_init_events.append(t_event)
 
     for g_event in game_events:
         if isinstance(g_event, sc2reader.events.game.CameraEvent):
             camera_events.append(g_event)
+        #account for moving terran bases
+        elif isinstance(g_event, sc2reader.events.game.TargetUnitCommandEvent) and (g_event.ability_name == "LandCommandCenter" or g_event.ability_name == "LandOrbitalCommand"):
+            unit_init_events.append(g_event)
+
+    return unit_init_events + camera_events
 
 
-def buildScoutingDictionaries_v2():
+def buildScoutingDictionaries(events):
+    '''Builds dictionaries where the keys are the frame and the value is the state of
+       scouting. "No scouting" indicates the team/player is not looking at any bases,
+       "Scouting themself" indicates the team/player is looking at their own base, and
+       "Scouting opponent" indicates the team/player is looking at their opponent's base'''
+
+    team1 = 1
+    team2 = 2
+
+    team1_scouting_states = {}
+    team2_scouting_states = {}
+
+    # Dictionaries of the locations of bases where the keys are unit ids
+    # and the values are locations (as tuples of (x, y) coordinates)
+    team1_bases = {}
+    team2_bases = {}
+
+    #Used for updating the scouting dictionaries
     prev_state1 = "Viewing themself"
     prev_frame1 = 0
     prev_state2 = "Viewing themself"
     prev_frame2 = 0
 
-    for event in sorted(unit_init_events + camera_events, key=lambda e: e.frame):
+    for event in sorted(events, key=lambda e: e.frame):
         i = event.frame
-        #accounting for Terran bases moving
-        if isinstance(event, sc2reader.events.tracker.UnitTypeChangeEvent):
-            #UnitTypeChangeEvents do not have locations, so I need to figure out
-            #some other way of finding the new location
-            if(event.unit.owner == r.players[0]):
-                team1_bases[event.unit_id] = event.location
-            elif(event.unit.owner == r.players[1]):
-                team2_bases[event.unit_id] = event.location
         #accounting for new bases
-        elif isinstance(event, sc2reader.events.tracker.TrackerEvent):
+        if isinstance(event, sc2reader.events.tracker.TrackerEvent):
             if (event.control_pid == team1) and not(event.unit_id in team1_bases):
                 team1_bases[event.unit_id] = event.location
             elif(event.control_pid == team2) and not(event.unit_id in team2_bases):
                 team2_bases[event.unit_id] = event.location
+        #accounting for Terran bases moving
+        elif isinstance(event, sc2reader.events.game.TargetUnitCommandEvent):
+            if(event.player.pid == team1):
+                team1_bases[event.target_unit_id] = event.location
+            elif(event.player.pid == team2):
+                team2_bases[event.target_unit_id] = event.location
         #checking camera events
         else:
             player = event.player.pid
@@ -127,21 +116,24 @@ def buildScoutingDictionaries_v2():
                     team2_scouting_states[i] = "No scouting"
                     prev_frame2 = i
                     prev_state2 = "No scouting"
+    return team1_scouting_states, team2_scouting_states
+
 
 def withinDistance(location, list):
-    #returns true if location is within a distance of any locations in base dictionary
+    '''Returns true if input location is within a distance of any locations in
+       base dictionary'''
     loc_x, loc_y = location[0], location [1]
     keys = list.keys()
     for key in keys:
         loc = list[key]
         x, y = loc[0], loc[1]
         distance_apart = math.sqrt((loc_x - x)**2 + (loc_y - y)**2)
-        if distance_apart <= 20:
+        if distance_apart <= 25:
             return True
     return False
 
 def updatePrevScoutStates(scouting_dict, frame, prev_frame, prev_state):
-    #updates all frames after prev_frame and before current frame to the prev_state
+    '''Updates all frames after prev_frame and before current frame to the prev_state'''
     if(prev_frame >= frame):
         return;
 
@@ -150,11 +142,14 @@ def updatePrevScoutStates(scouting_dict, frame, prev_frame, prev_state):
         scouting_dict[i] = prev_state
         i += 1
 
-def toTime(time_dict, scouting_dict):
+def toTime(scouting_dict, frames, seconds):
+    '''Creates a formatted dictionary of the time of game when a player's
+        scouting state changes. Most useful for verification and testing.'''
     length = len(scouting_dict.keys())
+    time_dict = {}
 
     state = scouting_dict[1]
-    time = (1/frames)*(r.length.seconds)
+    time = (1/frames)*(seconds)
     minStr = "{:2d}".format(int(time//60))
     secStr = "{:05.2f}".format(time%60)
     timeStr = minStr + ":" + secStr
@@ -164,41 +159,41 @@ def toTime(time_dict, scouting_dict):
     while(frame <= length):
         if scouting_dict[frame] != state:
             state = scouting_dict[frame]
-            time = (frame/frames)*(r.length.seconds)
+            time = (frame/frames)*(seconds)
             minStr = "{:2d}".format(int(time//60))
             secStr = "{:05.2f}".format(time%60)
             timeStr = minStr + ":" + secStr
             time_dict[timeStr] = state
         frame += 1
+    return time_dict
 
 def printTime(time_dict):
+    '''Used to neatly print a time dictionary in an easy to read way.'''
     keys = time_dict.keys()
     for key in keys:
         print(key, end = "")
         print(" -> ", end = "")
         print(time_dict[key])
 
-def main():
-    buildEventDictionaries()
-    buildScoutingDictionaries_v2()
+def detect_scouting(filename):
+    r = sc2reader.load_replay(filename)
+    tracker_events = r.tracker_events
+    game_events = r.game_events
+    frames = r.frames
+    seconds = r.length.seconds
 
+    allEvents = buildEventDictionaries(tracker_events, game_events)
+    team1_scouting_states, team2_scouting_states = buildScoutingDictionaries(allEvents)
+
+    team1_times = toTime(team1_scouting_states, frames, seconds)
+    team2_times = toTime(team2_scouting_states, frames, seconds)
 
     print("\n--------Team 1---------")
     #print(team1_scouting_states)
-    toTime(team1_times, team1_scouting_states)
     printTime(team1_times)
+
     print("\n\n--------Team 2---------")
     #print(team2_scouting_states)
-    toTime(team2_times, team2_scouting_states)
     printTime(team2_times)
 
-
-main()
-
-
-# Not worrying about this right now, but we need to add a way to delete bases
-# once they are destroyed.
-
-# Lastly, don't you need a unit with you while your camera is moving to
-# properly scout? Also, how will we tell the difference between scouting and
-# a battle? Will we set a threshold of the amount of players in motion?
+detect_scouting("replays/ggtracker_341316.SC2Replay")
