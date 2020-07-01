c360b74abebcd1cfd702f200ffd8f21bcf69af80
acameron13 <63213231+acameron13@users.noreply.github.com>
2020-07-01 14:09:47 -0700

    Some multiprocessing added with a few issues

--- starcraft/scouting_stats.py
+++ starcraft/scouting_stats.py
@@ -15,20 +15,19 @@ def generateFields(filename):
         fields = (game_id, team1_nums, team1_fraction, team2_nums, team2_fraction, winner)
         return fields
     except RuntimeError:
-        return 0
+        return
 
 if __name__ == "__main__":
     files = os.listdir("replays")
     pool = Pool()
-    results = pool.map(generateFields, files)
+    results = pool.imap_unordered(generateFields, files)
     pool.close()
     pool.join()
-    with open("scouting_stats.csv", 'w', newline = '') as fp:
+    with open("scouting_stats2.csv", 'w', newline = '') as fp:
         events_out = csv.DictWriter(fp, fieldnames=["Game ID", "Team 1 Scouting Frequency", "Team 1 Scouting Time",
                                                     "Team 2 Scouting Frequency", "Team 2 Scouting Time", "Winner (team #)"])
         events_out.writeheader()
         for fields in results:
-            if fields != 0:
-                events_out.writerow({"Game ID": fields[0], "Team 1 Scouting Frequency": fields[1], "Team 1 Scouting Time": fields[2],
-                                    "Team 2 Scouting Frequency": fields[3], "Team 2 Scouting Time": fields[4],
-                                    "Winner (team #)": fields[5]})
+            events_out.writerow({"Game ID": fields[0], "Team 1 Scouting Frequency": fields[1], "Team 1 Scouting Time": fields[2],
+                                "Team 2 Scouting Frequency": fields[3], "Team 2 Scouting Time": fields[4],
+                                "Winner (team #)": fields[5]})
