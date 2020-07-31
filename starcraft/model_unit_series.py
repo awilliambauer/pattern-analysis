ea1bd31de5b7b9dc7d2eb57d418f7d30faa8e76a
Aaron Bauer <awb@carleton.edu>
2020-07-31 14:07:06 -0500

    updated model_unit_series.py with sampling code, progress display, and parallel combine series

--- starcraft/model_unit_series.py
+++ starcraft/model_unit_series.py
@@ -3,6 +3,7 @@ Applying pattern extraction to series of current unit and building counts
 """
 from multiprocessing import Pool
 import traceback
+import argparse
 import numpy as np
 import sc2reader
 import os
@@ -136,29 +137,66 @@ def replay_to_series(replay_file: str, binsize: int) -> Optional[Dict[Tuple[str,
         return
 
 
-if __name__ == "__main__":
-    with open("valid_game_ids.txt") as fp:
-        replay_files = [x.strip() for x in fp.readlines() if x.startswith("spawningtool")]
-    with Pool() as pool:
-        series = pool.map(partial(replay_to_series, binsize=160), [f"replays/{x}" for x in replay_files])
+def make_combined_series(race, series_lookup):
+    results_path = f"results/{race}"
+    os.makedirs(results_path, exist_ok=True)
 
-    # sort series k-v pairs by race
-    series = sorted([(k, v) for d in [x for x in series if x] for k, v in d.items()], key=lambda x: x[0][2])
-    race_lookup = {race: dict(vs) for race, vs in groupby(series, lambda x: x[0][2])}
+    print("combining series for", race)
+    idx_lookup, all_series = combine_user_series(series_lookup, np.array([-1] * len(field_lookup[race])), 100)
+
+    np.savetxt(f"{results_path}/all_series.txt", all_series)
+    with open(f"{results_path}/idx_lookup.pickle", 'wb') as fp:
+        pickle.dump(idx_lookup, fp)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("model_unit_series.py")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--lookup")
+    group.add_argument("--sample", nargs=1, type=int)
+    args = parser.parse_args()
+
+    if args.lookup:
+        with open(args.lookup, "rb") as fp:
+            race_lookup = pickle.load(fp)
+    else:
+        with open("valid_game_ids.txt") as fp:
+            replay_files = [x.strip() for x in fp.readlines() if x.startswith("spawningtool")]
+        if args.sample:
+            matchups = []
+            metas = {}
+            rng = np.random.default_rng(42)
+            for rf in replay_files:
+                with open(f"{rf.replace('.SC2Replay', '_meta.json')}")as fp:
+                    meta = json.load(fp)
+                    metas[rf.split("_")[1].split(".")[0]] = meta
+                    matchups.extend(meta['tags'].get("Matchup", ["Missing"]))
+
+            matchup_frac = {m: c / sum(matchups_count.values()) for m, c in matchups_count.items()}
+            weights = np.array([matchup_frac[m["tags"]["Matchup"][0]] for m in metas.values()])
+            gids = rng.choice(list(metas.keys()), args.sample, False, weights / weights.sum())
+            replay_files = [f"spawningtool_{x}.SC2Replay" for x in gids]
+
+        series = []
+        with Pool() as pool:
+            for i, s in enumerate(pool.imap_unordered(partial(replay_to_series, binsize=160), [f"replays/{x}" for x in replay_files])):
+                print(f"done {i / len(replay_files):%}\r", end="")
+                if s:
+                    series.append(s)
+
+        # sort series k-v pairs by race
+        series = sorted([(k, v) for d in series for k, v in d.items()], key=lambda x: x[0][2])
+        race_lookup = {race: dict(vs) for race, vs in groupby(series, lambda x: x[0][2])}
+        with open("results/race_lookup.pickle", "wb") as fp:
+            pickle.dump(race_lookup, fp)
+
     all_series_lookup = {}
 
-    for race, series_lookup in race_lookup.items():
+    with Pool() as pool:
+        pool.starmap(make_combined_series, iter(race_lookup.items()))
 
-        results_path = f"results/{race}"
-        os.makedirs(results_path, exist_ok=True)
+    for race in race_lookup:
+        all_series_lookup[race] = np.loadtxt(f"results/{race}/all_series.txt")
 
-        print("combining series for", race)
-        idx_lookup, all_series = combine_user_series(series_lookup, np.array([-1] * len(field_lookup[race])), 100)
-
-        np.savetxt(f"{results_path}/all_series.txt", all_series)
-        all_series_lookup[race] = all_series
-        with open(f"{results_path}/idx_lookup.pickle", 'wb') as fp:
-            pickle.dump(idx_lookup, fp)
-            
     print("running ticc")
     run_TICC(all_series_lookup, "results", [20], window_size=5, num_proc=8)
