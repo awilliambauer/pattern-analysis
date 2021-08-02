7080987b343b6a9ae7d4bab1eafe0a11e1242517
David Chu <chud@carleton.edu>
2021-08-02 15:50:07 -0500

    update selection plugin

--- starcraft/selection_plugin.py
+++ starcraft/selection_plugin.py
@@ -7,3 +7,6 @@ class ActiveSelection():
 
     def handleControlGroupEvent(self, event, replay):
         event.active_selection = event.player.selection[10]
+        
+    def handleSelectionEvent(self, event, replay):
+        event.active_selection = event.player.selection[event.control_group]
